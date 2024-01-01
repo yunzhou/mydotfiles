74c8d6dc1a1247f1d5f8ba74a05bf875310ae17d
ZY0627 <ilikezhouyun@gmail.com>
2024-01-02 07:52:14 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #install nerd font
 curl -sS https://webi.sh/nerdfont | sh
 # link alacritty
-ln -s ~/src/mydotfiles/alacritty/alacritty.toml $HOME/alacritty.toml
+ln -s ~/src/mydotfiles/alacritty/alacritty.toml  $HOME/.config/alacritty/alacritty.toml
 
 #tmux config
 ln -s ~/src/mydotfiles/tmux/tmux.conf $HOME/.tmux.conf
