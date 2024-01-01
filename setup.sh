3ca5070d5bf0e37da03b187bf87b0e527e89cc8c
Yun Zhou <yzhou@squareup.com>
2024-01-01 19:33:06 +1100

    init commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#install nerd font
+curl -sS https://webi.sh/nerdfont | sh
+# link alacritty
+ln -s ~/src/mydotfiles/alacritty/alacritty.toml $HOME/alacritty.toml
+
+#tmux config
+ln -s ~/src/mydotfiles/tmux/tmux.conf $HOME/.tmux.conf
